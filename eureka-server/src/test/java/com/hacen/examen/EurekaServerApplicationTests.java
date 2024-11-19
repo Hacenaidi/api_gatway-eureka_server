fe4d1d0aea4473a197f2e02461cf53b914699c51
Hacenaidi <hacenaidi4455@gmail.com>
2024-11-19 15:09:53 +0100

    first commit

---
+++ eureka-server/src/test/java/com/hacen/examen/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacen.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
